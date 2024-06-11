14d3bf7392d957bfc546762b6d9767947203875e
BaptisteFredj <redj2304@gmail.com>
2024-06-11 15:58:46 +0200

    did the props exercice

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,6 +1,4 @@
-function PokemonCard() {
-  const pokemon = pokemonList[1];
-
+function PokemonCard({ pokemon }) {
   return (
     <>
       <figure>
@@ -12,14 +10,3 @@ function PokemonCard() {
 }
 
 export default PokemonCard;
-
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
